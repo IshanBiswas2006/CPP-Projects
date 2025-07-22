156adcf0f9cff21330ea7e953906442eb42db156
Ishan Biswas <biswas.ishan2006@gmail.com>
2025-07-22 18:24:32 +0530

    Add files via upload

---
+++ C++ Projects/Palindrome Verifier/Palindrome_Verifier.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+  bool isAlphaNum(char ch) {
+    return (ch >= '0' && ch <= '9') || (tolower(ch) >= 'a' && tolower(ch) <= 'z');
+  }
+
+  bool isPalindrome(string s) {
+     int st = 0, end = s.length() - 1;
+
+       while (st < end) {
+          if (!isAlphaNum(s[st])) {
+             st++;
+             continue;
+            }
+            if (!isAlphaNum(s[end])) {
+             end--;
+             continue;
+            }
+            if (tolower(s[st]) != tolower(s[end])) {
+             return false;
+            }
+            st++;
+            end--;
+        }
+        return true;
+    }
+
+int main ()
+{
+    string str;
+
+    cout<<"Enter Word: ";
+    getline(cin,str);
+
+    if(isPalindrome(str))
+    {
+        cout<<"Yes it's Palindrome: "<<str<<endl; 
+    }
+    else
+    {
+        cout<<"It's not Palindrome: "<<str<<endl; 
+    }
+
+}
