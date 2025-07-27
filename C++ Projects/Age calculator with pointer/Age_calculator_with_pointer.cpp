ac235d2a4de54607827d1fe5370f4ba64c6fca24
Ishan Biswas <biswas.ishan2006@gmail.com>
2025-07-27 20:14:38 +0530

    Add files via upload

---
+++ C++ Projects/Age calculator with pointer/Age_calculator_with_pointer.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+void age(int* c_d, int* c_m, int* c_y, int* d, int* m, int* y) {
+    int days, months, years;
+
+    if (*c_d < *d) {
+        *c_d += 30;
+        *c_m -= 1;
+    }
+
+    days = *c_d - *d;
+
+    if (*c_m < *m) {
+        *c_m += 12;
+        *c_y -= 1;
+    }
+
+    months = *c_m - *m;
+    years = *c_y - *y;
+
+    cout << "Your Age is: " << years << " years, " << months << " months, " << days << " days\n";
+}
+
+int main() {
+    int d, m, y;
+    int c_d, c_m, c_y;
+
+    cout << "Enter birth day, month, year: ";
+    cin >> d >> m >> y;
+
+    cout << "Enter current day, month, year: ";
+    cin >> c_d >> c_m >> c_y;
+
+ 
+    age(&c_d, &c_m, &c_y, &d, &m, &y);
+
+    return 0;
+}
